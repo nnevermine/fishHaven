671356b6052249df919ea329b9f62041a6c4a8f5
boblancer <62011192@kmitl.ac.th>
2022-04-29 14:25:43 +0700

    feat[fish photo in dashboard]

--- src/fishFrame.py
+++ src/fishFrame.py
@@ -1,14 +1,25 @@
-from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QLabel, QHBoxLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QSize
 
 class FishFrame(QGroupBox):
     
     def __init__(self, info, parent=None):
         super().__init__(parent)
         self.vbox = QVBoxLayout()
+        self.hbox = QHBoxLayout()
+        self.vbox.addLayout(self.hbox)
         self.setLayout(self.vbox)
         self.addInfo(info)
 
     def addInfo(self, info):
+        low_rez = QSize(100, 100)
+        label = QLabel(self)
+        pixmap = QPixmap('./assets/images/sprites/1.png')
+        pixmap = pixmap.scaled(low_rez)
+        label.setPixmap(pixmap)
+
+        self.addLabel(label)
         for i in info:
             self.addLabel(QLabel(i))
 
